95eea7297f659811e49a4a78701410ac83781e62
Sakif <sakifworkspace@gmail.com>
2025-03-29 20:01:37 +0600

    message

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import jobBanner from "../../assets/images/job-banner.jpg"
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
@@ -21,7 +22,7 @@ const Banner = () => {
             Each month, more than 3 million job seekers turn to website in their
             search for work, making over 140,000 applications every single day
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <Link to="/HotJobs"><button className="btn btn-primary">Get Started</button></Link>
         </div>
       </div>
     </div>
